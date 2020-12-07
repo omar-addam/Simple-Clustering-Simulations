6b7b6f92ed3097012312b89b84f86bd7f5e0bad1
omaddam <omaddam@gmail.com>
2020-12-07 01:24:12 -0700

    Creeated the k-mean implementation of the cluster class.

---
+++ Assets/Clustering/KMean/KMeanCluster.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Clustering.KMean
+{
+    [Serializable]
+    public class KMeanCluster : Core.Cluster
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Seed constructor
+        /// </summary>
+        public KMeanCluster(Core.Item seed)
+            : this(seed.Id, seed.PositionX, seed.PositionY)
+        {
+        }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="id">Unique identifier used to track clusters across multuple iterations.</param>
+        /// <param name="x">The x position of the center of the cluster.</param>
+        /// <param name="y">The y position of the center of the cluster.</param>
+        public KMeanCluster(Guid id, float x, float y)
+            : base(id, new List<Core.Item>())
+        {
+            X = x;
+            Y = y;
+        }
+
+        #endregion
+
+        #region Fields/Properties
+
+        /// <summary>
+        /// The x position of the center of the cluster.
+        /// </summary>
+        [SerializeField]
+        private float X = 0;
+
+        /// <summary>
+        /// The x position of the center of the cluster.
+        /// </summary>
+        public float CenterX { get { return X; } }
+
+
+
+        /// <summary>
+        /// The y position of the center of the cluster.
+        /// </summary>
+        [SerializeField]
+        private float Y = 0;
+
+        /// <summary>
+        /// The y position of the center of the cluster.
+        /// </summary>
+        public float CenterY { get { return Y; } }
+
+        #endregion
+
+    }
+}
