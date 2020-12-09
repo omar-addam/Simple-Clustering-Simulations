3020b7e7e2a22c50115c16308388e9ce80b69c5a
omaddam <omaddam@gmail.com>
2020-12-09 00:17:21 -0700

    Displayed seed items.

--- Assets/App/Scripts/GridSceneManager.cs
+++ Assets/App/Scripts/GridSceneManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using Clustering.Core;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridSceneManager : MonoBehaviour
@@ -16,6 +18,36 @@ public class GridSceneManager : MonoBehaviour
 	/// References the algorithm scriptable object.
 	/// </summary>
 	public AlgorithmManagerScriptableObject AlgorithmManager;
+
+	#endregion
+
+	#region Initialization
+
+	/// <summary>
+	/// Executes once on start.
+	/// </summary>
+	private void Awake()
+	{
+		// Display seed items and clusters
+		DisplaySeeds();
+	}
+
+	#endregion
+
+	#region Methods
+
+	/// <summary>
+	/// Displays the seed entities.
+	/// </summary>
+	private void DisplaySeeds()
+	{
+		// Clear all grid entities
+		GridManager.ClearEntities();
+
+		// Display seed items
+		List<Vector2> seedItems = AlgorithmManager.CurrentAlgorithm.AlgorithmItems.Select(x => new Vector2(x.PositionX, x.PositionY)).ToList();
+		GridManager.DisplayEntities(seedItems);
+	}
 
 	#endregion
 
