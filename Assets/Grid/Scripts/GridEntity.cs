149a10889bfba7cb3d8b26344e4caf0ac2e3eedc
omaddam <omaddam@gmail.com>
2020-12-09 01:16:36 -0700

    Added a method that sets the color of an entity.

--- Assets/Grid/Scripts/GridEntity.cs
+++ Assets/Grid/Scripts/GridEntity.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class GridEntity : MonoBehaviour
 {
     
+    /// <summary>
+    /// Sets the color of the entity.
+    /// </summary>
+    public void SetColor(Color color)
+    {
+        // Create a clone of the material
+        GetComponent<Renderer>().material = new Material(GetComponent<Renderer>().material);
+
+        // Set the color
+        GetComponent<Renderer>().material.color = color;
+    }
+
 }
