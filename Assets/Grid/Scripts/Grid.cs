aecf6b91cee0855cb1ce6cda33bc3b249fd99c8b
omaddam <omaddam@gmail.com>
2020-12-09 00:01:58 -0700

    Created a method that displays the entities on the grid.

--- Assets/Grid/Scripts/Grid.cs
+++ Assets/Grid/Scripts/Grid.cs
@@ -29,6 +29,23 @@ public class Grid : MonoBehaviour
 		foreach (Transform entity in EntitiesParent.transform)
 			GameObject.Destroy(entity.gameObject);
 	}
+	
+	/// <summary>
+	/// Displays entities on the grid.
+	/// </summary>
+	/// <param name="positions">Position of the entities.</param>
+	public void DisplayEntities(List<Vector2> positions)
+	{
+		// For each position, create an entity
+		foreach (Vector2 position in positions)
+		{
+			// Create a new entity instance
+			GameObject entity = Instantiate(EntityTemplate, new Vector3(position.x, position.y, 0), Quaternion.Euler(Vector3.zero), EntitiesParent.transform);
+
+			// Extract the script
+			GridEntity entityScript = entity.GetComponent<GridEntity>();
+		}
+	}
 
 	#endregion
 
