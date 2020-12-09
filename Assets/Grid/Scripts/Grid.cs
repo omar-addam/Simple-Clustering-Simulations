7d42c65719dc340e9ac4853c5f5579a53b6afc6b
omaddam <omaddam@gmail.com>
2020-12-08 23:44:08 -0700

    Referenced the entities parent and entity template in the grid script.

--- Assets/Grid/Scripts/Grid.cs
+++ Assets/Grid/Scripts/Grid.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class Grid : MonoBehaviour
 {
 
+	#region Variables
+
+	/// <summary>
+	/// References the prefab used to generate entities and display them on the grid.
+	/// </summary>
+	public GameObject EntityTemplate;
+
+	/// <summary>
+	/// References the gameobject that will hold all gw3e
+	/// </summary>
+	public GameObject EntitiesParent;
+
+	#endregion
+
 }
