c66254a232c921b649d0e23cbc85818d621a171f
omaddam <omaddam@gmail.com>
2020-12-09 00:06:11 -0700

    Referenced the grid in the scene and the algorithm scriptable object inside the scene manager.

--- Assets/App/Scripts/GridSceneManager.cs
+++ Assets/App/Scripts/GridSceneManager.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class GridSceneManager : MonoBehaviour
 {
 
+	#region Variables
 
+	/// <summary>
+	/// References the grid in the scene.
+	/// </summary>
+	public Grid GridManager;
+
+	/// <summary>
+	/// References the algorithm scriptable object.
+	/// </summary>
+	public AlgorithmManagerScriptableObject AlgorithmManager;
+
+	#endregion
 
 }
