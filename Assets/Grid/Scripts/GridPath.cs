e940f1d0c148db6deee772be6764943eb6bcb18b
omaddam <omaddam@gmail.com>
2020-12-09 20:25:12 -0700

    Created the grid path script and attached to the prefab.

---
+++ Assets/Grid/Scripts/GridPath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPath : MonoBehaviour
+{
+
+    /// <summary>
+    /// Sets the start and end points of the path.
+    /// </summary>
+    public void SetPath(Vector2 start, Vector2 end)
+    {
+        GetComponent<LineRenderer>().SetPositions
+        (
+            new Vector3[]
+            {
+                new Vector3(start.x, start.y, 0),
+                new Vector3(end.x, end.y, 0)
+            }
+        );
+    }
+
+}
