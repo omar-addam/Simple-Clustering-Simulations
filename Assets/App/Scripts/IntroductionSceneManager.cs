d1902f953e5fee9b7ea4bfa7c2200773ca302b45
omaddam <omaddam@gmail.com>
2020-12-12 04:46:48 -0700

    Created a better sample.

--- Assets/App/Scripts/IntroductionSceneManager.cs
+++ Assets/App/Scripts/IntroductionSceneManager.cs
@@ -4,8 +4,10 @@ using Clustering.KMeans;
 using Clustering.KMedoids;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.LowLevel;
 
 public class IntroductionSceneManager : MonoBehaviour
 {
@@ -140,7 +142,7 @@ public class IntroductionSceneManager : MonoBehaviour
 
 		// Select k-mean algorithm
 		AlgorithmManager.Clear();
-		AlgorithmManager.SelectDBScanAlgorithm(3, 2);
+		AlgorithmManager.SelectDBScanAlgorithm(2, 2);
 
 		// Create sample
 		GenerateDBScanSample((DBScanAlgorithm)AlgorithmManager.CurrentAlgorithm);
@@ -157,19 +159,36 @@ public class IntroductionSceneManager : MonoBehaviour
 	/// </summary>
 	private void GenerateDBScanSample(DBScanAlgorithm algorithm)
 	{
-		// Create items sample
-		algorithm.Items.Add(new Item(4, 5));
-		algorithm.Items.Add(new Item(3, 7));
-		algorithm.Items.Add(new Item(4.5f, 4));
-		algorithm.Items.Add(new Item(5, 6));
+		// Center
+		int centerWidth = 3;
+		for (int i = -centerWidth; i <= centerWidth; i++)
+		{
+			// Add + (horizontal and vertical lines passing through the center)
+			algorithm.Items.Add(new Item(i, 0));
+			algorithm.Items.Add(new Item(0, i));
 
-		algorithm.Items.Add(new Item(-1, -1));
-		algorithm.Items.Add(new Item(-4, -3));
-		algorithm.Items.Add(new Item(-4, -1.5f));
-		algorithm.Items.Add(new Item(-5, -1));
-		algorithm.Items.Add(new Item(-5, -2));
-		algorithm.Items.Add(new Item(-6, -1));
-		algorithm.Items.Add(new Item(-6, -2));
+			// Add /\ (diagonal lines passing through the center)
+			if (i > -centerWidth && i < centerWidth && i != 0)
+				algorithm.Items.Add(new Item(i, i));
+
+			// Add the rest
+			if (i > -centerWidth && i < centerWidth && i != 0)
+				algorithm.Items.Add(new Item(-i, i));
+		}
+
+		// Edge
+		int edgeStart = 7;
+		int edgeWidth = 2;
+		for (int i = -edgeStart; i <= edgeStart; i++)
+		{
+			for (int j = 0; j < edgeWidth; j++)
+			{
+				algorithm.Items.Add(new Item((edgeStart + j), i));
+				algorithm.Items.Add(new Item(-(edgeStart + j), i));
+				algorithm.Items.Add(new Item(i, (edgeStart + j)));
+				algorithm.Items.Add(new Item(i, -(edgeStart + j)));
+			}
+		}
 	}
 
 	#endregion
