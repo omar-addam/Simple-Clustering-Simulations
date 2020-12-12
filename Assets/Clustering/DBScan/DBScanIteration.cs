bd60dd2c359acf89adb04ae189fcea3bd77b8631
omaddam <omaddam@gmail.com>
2020-12-12 03:50:31 -0700

    Created a db scan iteration.

---
+++ Assets/Clustering/DBScan/DBScanIteration.cs
@@ -0,0 +1,43 @@
+﻿using Clustering.Core;
+using System.Collections.Generic;
+
+namespace Clustering.DBScan
+{
+    public class DBScanIteration : Iteration
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Copy constructor.
+        /// </summary>
+        public DBScanIteration(int order, DBScanIteration iteration)
+            : base(iteration)
+        {
+            _Order = order;
+
+            Pending = new List<Item>();
+            Pending.AddRange(iteration.Pending);
+
+            Noise = new List<Item>();
+            Noise.AddRange(iteration.Noise);
+        }
+
+        #endregion
+
+        #region Fields/Properties
+
+        /// <summary>
+        /// List of items pending to be clustered.
+        /// </summary>
+        public List<Item> Pending { private set; get; }
+
+        /// <summary>
+        /// List of items that do not belong to any cluster.
+        /// </summary>
+        public List<Item> Noise { private set; get; }
+
+        #endregion
+
+    }
+}
