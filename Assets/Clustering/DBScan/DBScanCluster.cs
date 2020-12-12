ca6f3eccd1886557587045b025c8e8e5d44c3396
omaddam <omaddam@gmail.com>
2020-12-12 02:03:04 -0700

    Created the db scan cluster.

---
+++ Assets/Clustering/DBScan/DBScanCluster.cs
@@ -0,0 +1,51 @@
+﻿using Clustering.Core;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Clustering.KMeans
+{
+    [Serializable]
+    public class DBScanCluster : Cluster
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Empty constructor.
+        /// </summary>
+        public DBScanCluster()
+            : base(new List<Item>())
+        {
+            _RecentlyAddedItems = new List<Item>();
+        }
+
+        /// <summary>
+        /// Clone constructor.
+        /// </summary>
+        public DBScanCluster(DBScanCluster cluster)
+            : base(cluster)
+        {
+            _RecentlyAddedItems = new List<Item>();
+        }
+
+        #endregion
+
+        #region Fields/Properties
+
+        /// <summary>
+        /// The list of items added in the last iteration.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("The list of items added in the last iteration.")]
+        private List<Item> _RecentlyAddedItems;
+
+        /// <summary>
+        /// The list of items added in the last iteration.
+        /// </summary>
+        public List<Item> RecentlyAddedItems { get { return _RecentlyAddedItems; } }
+
+        #endregion
+
+    }
+}
